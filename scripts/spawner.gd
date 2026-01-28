4b48b0ff0532e1911e502878a01567f7d96c4912
aznoqmous <paul.landreau931v@gmail.com>
2026-01-28 11:05:56 +0100

    re spawned

--- scripts/spawner.gd
+++ scripts/spawner.gd
@@ -1,17 +1,27 @@
 class_name Spawner extends Node2D
 
 @export var spawn_parent : Node2D
-@export var spawn_points : Array[Node2D]
+@export var spawn_starting_coins := 3.0
 @export var spawn_cooldown := 3.0
 @export var spawn_base_gain := 0.3
+@export var spawn_gain_increment := 0.1
+@export var on_spawn_cooldown := 1.0
+@export var spawn_points : Array[Node2D]
 @export var spawned_resources : Array[SpawnedResource]
 
+var last_spawn_point : Node2D
+var spawn_gain := 0.0
 var coins := 1.0
 var spawn_time := 0.0
+
+func _ready():
+	coins = spawn_starting_coins
+	spawn_gain = spawn_base_gain
 
 func _process(delta):
 	coins += delta * spawn_base_gain
 	spawn_time -= delta
+	spawn_gain += spawn_gain_increment * delta
 	if spawn_time < 0:
 		spawn()
 		
@@ -21,10 +31,12 @@ func spawn():
 	if available_resources.size() <= 0:
 		spawn_time = spawn_cooldown
 		return;
+	spawn_time = on_spawn_cooldown
 	var spawned : SpawnedResource = available_resources.pick_random()
 	coins -= spawned.cost
 	var spawned_instance = spawned.packed_scene.instantiate()
-	var spawn_point = spawn_points.pick_random()
+	var spawn_point = spawn_points.filter(func(a): return a != last_spawn_point).pick_random()
+	last_spawn_point = spawn_point
 	spawn_parent.add_child(spawned_instance)
 	spawned_instance.global_position = spawn_point.global_position
 	
