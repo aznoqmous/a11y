72b569130d1f466cd3f0a06af59b94b281532bd0
aznoqmous <paul.landreau931v@gmail.com>
2026-01-28 16:50:36 +0100

    moved racing spawners

--- scripts/overlay_control.gd
+++ scripts/overlay_control.gd
@@ -3,7 +3,7 @@ class_name OverlayControl extends CanvasLayer
 @onready var score_label: Label = $Control/ScoreLabel
 
 func _ready() -> void:
-	score_label.text = str(GameManager.score)
+	score_label.text = str(int(GameManager.score))
 	GameManager.score_update.connect(func(value):
 		score_label.text = str(int(value))
 	)
