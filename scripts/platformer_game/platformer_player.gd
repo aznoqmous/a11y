b57f4a804174532c18eeb47c8a3ed5f10cef4aff
aznoqmous <paul.landreau931v@gmail.com>
2026-01-29 16:19:38 +0100

    title scene

--- scripts/platformer_game/platformer_player.gd
+++ scripts/platformer_game/platformer_player.gd
@@ -10,9 +10,11 @@ var is_jumping = true
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("Space") and is_coyote_floored():
 		jump()
+		
 func jump():
 	velocity.y -= jump_power
 	coyote_time = 0.0
+	
 func _process(delta: float) -> void:
 	GameManager.set_access_color(sprite_2d, GameManager.Colors.PLAYER)
 	if Input.is_action_pressed("Left"):
