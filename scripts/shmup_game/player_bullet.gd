c128e78b7a4576bfb39ea4fd33591861227e62f9
aznoqmous <paul.landreau931v@gmail.com>
2026-01-30 09:58:21 +0100

    clip foe and bullets in shmup

--- scripts/shmup_game/player_bullet.gd
+++ scripts/shmup_game/player_bullet.gd
@@ -12,3 +12,5 @@ func _ready() -> void:
 func _process(delta):
 	GameManager.set_access_color(sprite_2d, GameManager.Colors.PLAYER)
 	position += speed * delta
+	if position.distance_to(get_viewport_rect().size / 2) > get_viewport_rect().size.length():
+		queue_free()
